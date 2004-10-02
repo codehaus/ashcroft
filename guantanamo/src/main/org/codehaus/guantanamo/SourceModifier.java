1a105f8ea9eb638fc72218b81db6e63df6d4aae3
rinkrank <rinkrank@b6d8753e-d0e1-0310-8f6f-e61c8a77645a>
2004-10-02 01:57:01 +0000

    Updated copyright

--- guantanamo/src/main/org/codehaus/guantanamo/SourceModifier.java
+++ guantanamo/src/main/org/codehaus/guantanamo/SourceModifier.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (C) ModifyingSourceVisitor Organization. All rights reserved.               *
+ * Copyright (C) Guantanamo Organization. All rights reserved.               *
  * ------------------------------------------------------------------------- *
  * The software in this package is published under the terms of the BSD      *
  * style license a copy of which has been included with this distribution in *
