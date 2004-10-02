187541db3a011b48356324be9299331f68c4de74
rinkrank <rinkrank@b6d8753e-d0e1-0310-8f6f-e61c8a77645a>
2004-10-02 06:54:55 +0000

    Remove big headers

--- guantanamo/src/testdata/org/codehaus/guantanamo/testdata/PoorlyTestedTest.java
+++ guantanamo/src/testdata/org/codehaus/guantanamo/testdata/PoorlyTestedTest.java
@@ -1,21 +1,7 @@
-/*****************************************************************************
- * Copyright (C) Guantanamo Organization. All rights reserved.               *
- * ------------------------------------------------------------------------- *
- * The software in this package is published under the terms of the BSD      *
- * style license a copy of which has been included with this distribution in *
- * the LICENSE.txt file.                                                     *
- *                                                                           *
- * Original code by Aslak Hellesoy                                           *
- * Idea by Chris Stevenson                                                   *
- *****************************************************************************/
 package org.codehaus.guantanamo.testdata;
 
 import junit.framework.TestCase;
 
-/**
- * @author Aslak Helles&oslash;y
- * @version $Revision$
- */
 public class PoorlyTestedTest extends TestCase {
     private PoorlyTested poorlyTested = new PoorlyTested();
 
